25d7e4ec29d8ca478917bcb2ddbce36c393e6d70
mozuku28 <mozuku28@gmail.com>
2018-09-20 15:16:20 +0000

    test commit

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,20 +5,20 @@ class ProductsController < ApplicationController
 		@product = Product.find(params[:id])
 	end
 
-    def index
-    	productx = Product.search(params[:search])
-        @products = productx.page(params[:page]).reverse_order
-    end
+  def index
+  	productx = Product.search(params[:search])
+      @products = productx.page(params[:page]).reverse_order
+  end
 
-    def create
-        cart = Cart.new(cart_params)
-        cart.save
-        redirect_to carts_path
-    end
+  def create
+      cart = Cart.new(cart_params)
+      cart.save
+      redirect_to carts_path
+  end
 
-    def search
-    	@products = Product.search(params[:search])
-    	redirect_to products_path
-    end
+  def search
+  	@products = Product.search(params[:search])
+  	redirect_to products_path
+  end
 
 end
