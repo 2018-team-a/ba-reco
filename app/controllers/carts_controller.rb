c419772a9043ca1fe2fb35f6ee9a5428bed235bd
tomytomyk <to6-mi25.ta43@ezweb.ne.jp>
2018-09-25 05:04:42 +0000

    purchase

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -30,13 +30,14 @@ class CartsController < ApplicationController
 	end
 
 	def cart_last
-		puts "aaaaaa"
-			cart = Cart.find(params[:id])
-			purchase = Purchase.new(user_id: current_user.id, destination_id: params[:destination_id])
-			purchases.user_id = current_user.id
-
-			purchases.save
+			carts = Cart.where(params[:id])
+			purchase = Purchase.new(user_id: current_user.id, total_price: @price, destination_id: purchase_params[:destination_id], status: "準備中")
+            purchase.save
+            carts.each do |cart|
+            purchase_single = PurchaseSingle.new(purchase_id: purchase.id, product_id: cart.product_id, sheet_number: cart.sheet_number)
+            purchase_single.save
 			cart.destroy
+		end
 			redirect_to root_path
 	end
 
@@ -62,20 +63,22 @@ class CartsController < ApplicationController
 			redirect_to carts_path
 	end
 
-
 # ex: def (ここでform受け取って2個の処理をしてtopにredirectさせる)
 # end
 
 	private
+	    # cart_params[]
 
 		def cart_params
         	params.require(:cart).permit(:product_id, :sheet_number, :user_id)
     	end
 
-    	def cart_last
-        	params.require(:purchase).permit(:destination_id)
+    	def purchase_params
+        	params.require(:purchase).permit(:destination_id, :status, :user_id, :total_price)
     	end
-
+    	def purchase_single_params
+            params.require(:purchase_single).permit(:purchase_id, :product_id, :sheet_numer)
+    	end
     	# def cart_ccc
     	# 	params.require(:user).permit(:user_id)
     	# end
