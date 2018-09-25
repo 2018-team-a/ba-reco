aeaa5229cb4665c12d2ca36efd07f3db9b539c07
mozuku28 <mozuku28@gmail.com>
2018-09-25 08:07:13 +0000

    hoge

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2018_09_24_143639) do
 
   create_table "admins", force: :cascade do |t|
