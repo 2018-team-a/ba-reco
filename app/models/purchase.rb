3b0f69243bd9e6ba27de36e92cdefb621367e7d3
mozuku28 <mozuku28@gmail.com>
2018-09-26 06:22:07 +0000

    eunm 追加　残作業修正中

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -4,6 +4,8 @@ class Purchase < ApplicationRecord
 	belongs_to :user
 	belongs_to :destination
 
+	enum status: { 受付: 0, 商品準備中: 1, 出荷済: 2 }
+
 	def self.search(search)
 		if search
 			Purchase.where(['created_at LIKE ?', "%#{search}%"])
